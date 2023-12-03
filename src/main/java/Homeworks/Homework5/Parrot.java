e4fd8cc2b40b7879878dd13beb7048da24238553
Zhelyazko <jp.shahanov@gmail.com>
2023-12-03 14:37:15 +0300

    Homework 5 v2 with geter and seter

--- src/main/java/Homeworks/Homework5/Parrot.java
+++ src/main/java/Homeworks/Homework5/Parrot.java
@@ -1,25 +1,38 @@
 package Homeworks.Homework5;
 
 public class Parrot extends Pet{
-    public boolean hasTail; //specific characteristics for the Parrot
+    //instance variable
+    private boolean hasTail; //specific characteristics for the Parrot
+
+    //properties
+
+
+    public boolean isHasTail() {
+        return hasTail;
+    }
+
+    public void setHasTail(boolean hasTail) {
+        this.hasTail = hasTail;
+    }
+
     //constructors
     public Parrot(String breed, String sex, byte age, String color, float weight,
                   String name, byte legs, float height, boolean fur, boolean hasTail){
         super(breed, sex, age, color, weight, name, legs, height, fur);//calling constructor of the super (or parent) class
-        this.hasTail = hasTail;
+        setHasTail(hasTail);
     }
 
 
     //methods i.e. actions
     @Override
     public void makeSomeNoise() {
-        System.out.printf("%s is talking...\n", name);
+        System.out.printf("%s is talking...\n", getName());
     }
 
     public void printData(){
         System.out.printf("%s is %d years old, has %f height and weights %f kg. It is from %s sex, the color is %s" +
-                ", the breed is %s. It has %d legs and", name, age, height, weight, sex, color, breed, legs);
-        if (fur) {
+                ", the breed is %s. It has %d legs and", getName(), getAge(), getHeight(), getWeight(), getSex(), getColor(), getBreed(), getLegs());
+        if (isFur()) {
             System.out.println(" has big feathers.");
         } else {
             System.out.println(" does not have big feathers.");
--- src/main/java/Homeworks/Homework5/Pet.java
+++ src/main/java/Homeworks/Homework5/Pet.java
@@ -1,27 +1,134 @@
 package Homeworks.Homework5;
 
 public class Pet {
-    public String breed;
-    public String sex;
-    public byte age;
-    public String color;
-    public float weight;
-    public String name;
-    public byte legs;
-    public float height;
-    public boolean fur;
+    private String breed;
+    private String sex;
+    private byte age;
+    private String color;
+    private float weight;
+    private String name;
+    private byte legs;
+    private float height;
+    private boolean fur;
+
+    //properties
+
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        if(!breed.isEmpty()){
+            this.breed = breed;
+        }else {
+            System.out.println("Error: Invalid entry for breed!");
+        }
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        if(!sex.isEmpty()){
+            this.sex = sex;
+        }else {
+            System.out.println("Error: Invalid entry for sex!");
+        }
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        if(age >= 0){
+            this.age = age;
+        }else {
+            System.out.println("Error: Invalid entry for age!");
+        }
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if(!color.isEmpty()){
+            this.color = color;
+        }else {
+            System.out.println("Error: Invalid entry for color!");
+        }
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        if(weight >= 0.05f){
+            this.weight = weight;
+        }else {
+            System.out.println("Error: Invalid entry for weight!");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(!name.isEmpty()){
+            this.name = name;
+        }else {
+            System.out.println("Error: Invalid entry for name!");
+        }    }
+
+    public byte getLegs() {
+        return legs;
+    }
+
+    public void setLegs(byte legs) {
+        if(legs >= 1){
+            this.legs = legs;
+        }else {
+            System.out.println("Error: Invalid entry for legs!");
+        }
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        if(height >= 3){
+            this.height = height;
+        }else {
+            System.out.println("Error: Invalid entry for height!");
+        }
+    }
+
+    public boolean isFur() {
+        return fur;
+    }
+
+    public void setFur(boolean fur) {
+        this.fur = fur;
+    }
 
     //constructors
     public Pet(String breed, String sex, byte age, String color, float weight, String name, byte legs, float height, boolean fur){
-        this.breed = breed;
-        this.sex = sex;
-        this.age = age;
-        this.color = color;
-        this.weight = weight;
-        this.name = name;
-        this.legs = legs;
-        this.height = height;
-        this.fur = fur;
+        setBreed(breed);
+        setSex(sex);
+        setAge(age);
+        setColor(color);
+        setWeight(weight);
+        setName(name);
+        setLegs(legs);
+        setHeight(height);
+        setFur(fur);
     }
 
     //default constructor
